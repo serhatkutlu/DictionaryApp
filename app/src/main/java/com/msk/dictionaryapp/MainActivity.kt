491c58e6ac31459ca981da8ee9f508992f98100a
serhatkutlu <84921644+serhatkutlu@users.noreply.github.com>
2022-02-09 11:32:13 +0300

    user interface created

--- app/src/main/java/com/msk/dictionaryapp/MainActivity.kt
+++ app/src/main/java/com/msk/dictionaryapp/MainActivity.kt
@@ -3,36 +3,78 @@ package com.msk.dictionaryapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.*
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.msk.dictionaryapp.feature.presentation.wordInfoItem
+import com.msk.dictionaryapp.feature.presentation.wordInfoViewModel
 import com.msk.dictionaryapp.ui.theme.DictionaryAppTheme
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             DictionaryAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                val viewModel: wordInfoViewModel = hiltViewModel()
+                val state = viewModel.state.value
+                val scaffoldState = rememberScaffoldState()
+                LaunchedEffect(key1 = true) {
+                    viewModel.eventFlow.collectLatest { event ->
+                        when (event) {
+                            is wordInfoViewModel.UIEvent.ShowSnackbar -> {
+                                scaffoldState.snackbarHostState.showSnackbar(
+                                    message = event.message
+                                )
+                            }
+                        }
+
+                    }
+                }
+
+                Scaffold(scaffoldState = scaffoldState) {
+                    Box(modifier = Modifier.background(MaterialTheme.colors.background)) {
+                        Column(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(16.dp)
+                        ) {
+                            TextField(value = viewModel.searchQuery.value,
+                                onValueChange =viewModel::onsearch,
+                                modifier = Modifier.fillMaxWidth(),
+                                placeholder = {
+                                    Text(text = "Search...")
+                                })
+
+                        Spacer(modifier = Modifier.height(16.dp))
+                        LazyColumn(modifier = Modifier.fillMaxSize()) {
+                            items(state.wordInfoItem.size) { i ->
+                                val wordInfo = state.wordInfoItem[i]
+                                if (i > 0) {
+                                    Spacer(modifier = Modifier.height(8.dp))
+                                }
+                                wordInfoItem(wordInfo = wordInfo)
+                                if (i < state.wordInfoItem.size - 1) {
+                                    Divider()
+                                }
+
+                            }
+                        }}
+                        if (state.isloading) {
+                            CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+                    }
+
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    DictionaryAppTheme {
-        Greeting("Android")
     }
 }
